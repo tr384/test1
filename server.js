a96c81638703ae8ea866beea85595da305f36d22
tr384 <tr384@live.mdx.ac.uk>
2023-02-02 22:32:58 +0000

    update mongo

--- server.js
+++ server.js
@@ -1,94 +1,46 @@
-const { MongoClient, ObjectId } = require("mongodb");
-// establish a connection with mongodb
-const uri =
-  "mongodb+srv://user:123@cluster0.gbu5ohw.mongodb.net/?retryWrites=true&w=majority"; //uri for mongodb
-var client = new MongoClient(uri);
-
-async function connect() {
-  // try to establish a connection with the mongodb
-  try {
-    await client.connect().then(() => console.log("connected to mongodb"));
-  } catch (e) {
-    console.log("error while connecting to mongodb", e);
-  }
-}
-
-connect();
-
-// defining all the functions responsible for contacting mongodb and doing database transactions
-async function createOrder(order) {
-	return await client.db("project").collection("orders").insertOne(order);
-}
-
-async function getLessons() {
-	return client
-    .db("project")
-    .collection("lessons")
-    .find().toArray();
-}
-
-async function updateLesson(id, space) {
-	return await client
-    .db("project")
-    .collection("lessons")
-    .updateOne({ _id: ObjectId(id) }, { $inc: { "space": -space } });
-}
-
-async function searchLesson(searchTerm) {
-return client
-  .db("project")
-  .collection("lessons")
-  .find({
-    topic: { $regex: searchTerm, $options: "is" },
-  })
-  .toArray();
-}
-
-// setting up express server
+const path = require("path");
 const express = require("express");
-var cors = require("cors");
 const app = express();
+
+
+let propertiesReader = require("properties-reader");
+let propertiesPath = path.resolve(__dirname, "conf/db.properties");
+let properties = propertiesReader(propertiesPath);
+let dbPprefix = properties.get("db.prefix");
+//URL-Encoding of User and PWD
+//for potential special characters
+let dbUsername = encodeURIComponent(properties.get("db.user"));
+let dbPwd = encodeURIComponent(properties.get("db.pwd"));
+let dbName = properties.get("db.dbName");
+let dbUrl = properties.get("db.dbUrl");
+let dbParams = properties.get("db.params");
+const uri = dbPprefix + dbUsername + ":" + dbPwd + dbUrl + dbParams;
+
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const client = new MongoClient(uri, { serverApi: ServerApiVersion.v1 });
+let db = client.db(dbName);
+
+app.use(cors());
+
 app.use(express.json());
-app.use(cors());  // enabling CORS to avoid CORS error between frontend and backend
 
-// defining middlewares
-const logger = function (req, res, next) {
-  console.log(`Request for ${req.originalUrl}`);
-  next();
-};
+//app.set('json spaces', 3);
 
-// registering middlewares
-app.use(logger);
-app.use("/public", express.static(__dirname + "/public"));  // inbuild "static" middleware to serve course images
+ app.param('collectionName'
+    , function (req, res, next, collectionName) {
+      req.collection = db.collection(collectionName);
+      return next();
+   });
 
-// Defining api routes
-app.get("/api/lesson", async (req, res) => {
-  const result = await getLessons();
-  res.send(result);
-});
-
-app.post("/api/order", async (req, res) => {
-	const result = await createOrder(req.body);
-  res.send({
-		msg: `Reservation with id [${result.insertedId}] has been created successfully!`,
-	});
-});
-
-app.put("/api/lesson/:id", async(req, res) => {
-	const result = await updateLesson(req.params.id, req.body.space);
-	res.send({
-    msg: `Spaces in the lesson [id: ${req.params.id}] updated after successful order`,
-  });
-});
-
-app.get("/api/search/:searchTerm", async (req, res) => {
-  const result = await searchLesson(req.params.searchTerm);
-  res.send(result);
-});
-
-app.get("/", async (req, res) => {
-  res.sendFile(__dirname +"/index.html");
-});
+app.get('/collections/:collectionName'
+   , function (req, res, next) {
+      req.collection.find({}).toArray(function (err, results) {
+         if (err) {
+            return next(err);
+         }
+         res.send(results);
+      });
+   });
 
 // PORT
 const PORT = process.env.PORT || 3000;
